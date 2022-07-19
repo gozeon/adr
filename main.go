7140bcd5f68e9928538137e4da06201974f73acf
gozeon <goze.qiu@gmail.com>
2022-07-19 21:37:34 +0800

    init record

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"adr/model"
 	"adr/ui"
-	"fmt"
 	"net/http"
 
 	"github.com/go-playground/validator/v10"
@@ -24,7 +23,7 @@ func main() {
 		panic(err)
 	}
 
-	db.AutoMigrate(&model.Project{})
+	db.AutoMigrate(&model.Project{}, &model.Record{}, &model.Comment{})
 
 	app := fiber.New(fiber.Config{
 		// https://docs.gofiber.io/guide/error-handling
@@ -54,12 +53,11 @@ func main() {
 	// https://docs.gofiber.io/guide/grouping
 	api := app.Group("/api")
 
-	api.Get("/project", func(c *fiber.Ctx) error {
+	api.Get("/projects", func(c *fiber.Ctx) error {
 		var json []model.Project
 
-		err := db.Model(&model.Project{}).Order("updated_at desc").Find(&json)
-		if err != nil {
-			fmt.Println(err)
+		if err := db.Model(&model.Project{}).Order("updated_at desc").Find(&json).Error; err != nil {
+			return err
 		}
 
 		return c.Status(200).JSON(fiber.Map{
@@ -96,6 +94,132 @@ func main() {
 	api.Delete("/project/:id", func(c *fiber.Ctx) error {
 
 		if err := db.Delete(&model.Project{}, c.Params("id")).Error; err != nil {
+			return err
+		}
+
+		return c.Status(200).JSON(fiber.Map{
+			"errNo": 0,
+		})
+	})
+
+	api.Get("/records", func(c *fiber.Ctx) error {
+		var json []model.Record
+
+		if err := db.Model(&model.Record{}).Order("created_at asc").Find(&json).Error; err != nil {
+			return err
+		}
+
+		return c.Status(200).JSON(fiber.Map{
+			"errNo": 0,
+			"data":  json,
+		})
+	})
+
+	api.Get("/record/:project_id", func(c *fiber.Ctx) error {
+		var json []model.Record
+
+		if err := db.Model(&model.Record{}).Where("project_id = ?", c.Params("project_id")).Order("created_at asc").Find(&json).Error; err != nil {
+			return err
+		}
+
+		return c.Status(200).JSON(fiber.Map{
+			"errNo": 0,
+			"data":  json,
+		})
+	})
+
+	api.Post("/record", func(c *fiber.Ctx) error {
+		json := new(model.Record)
+		if err := c.BodyParser(json); err != nil {
+			return err
+		}
+
+		if err := validator.New().Struct(json); err != nil {
+			return err
+		}
+
+		// TODO: valid project_id exisit
+
+		// create or updte
+		if err := db.Model(&model.Record{}).Where("id = ?", json.ID).Save(json).Error; err != nil {
+			if err == gorm.ErrRecordNotFound {
+				db.Create(json)
+			} else {
+				return err
+			}
+		}
+
+		return c.Status(200).JSON(fiber.Map{
+			"errNo": 0,
+			"data":  json,
+		})
+	})
+
+	api.Delete("/record/:id", func(c *fiber.Ctx) error {
+		if err := db.Delete(&model.Record{}, c.Params("id")).Error; err != nil {
+			return err
+		}
+
+		return c.Status(200).JSON(fiber.Map{
+			"errNo": 0,
+		})
+	})
+
+	api.Get("/comment", func(c *fiber.Ctx) error {
+		var json []model.Comment
+
+		if err := db.Model(&model.Comment{}).Order("created_at asc").Find(&json).Error; err != nil {
+			return err
+		}
+
+		return c.Status(200).JSON(fiber.Map{
+			"errNo": 0,
+			"data":  json,
+		})
+	})
+
+	api.Get("/comment/:record_id", func(c *fiber.Ctx) error {
+		var json []model.Comment
+
+		if err := db.Model(&model.Comment{}).Where("record_id = ?", c.Params("record_id")).Order("created_at asc").Find(&json).Error; err != nil {
+			return err
+		}
+
+		return c.Status(200).JSON(fiber.Map{
+			"errNo": 0,
+			"data":  json,
+		})
+	})
+
+	api.Post("/comment", func(c *fiber.Ctx) error {
+		json := new(model.Comment)
+		if err := c.BodyParser(json); err != nil {
+			return err
+		}
+
+		if err := validator.New().Struct(json); err != nil {
+			return err
+		}
+
+		// TODO: valid project_id exisit
+
+		// create or updte
+		if err := db.Model(&model.Comment{}).Where("id = ?", json.ID).Save(json).Error; err != nil {
+			if err == gorm.ErrRecordNotFound {
+				db.Create(json)
+			} else {
+				return err
+			}
+		}
+
+		return c.Status(200).JSON(fiber.Map{
+			"errNo": 0,
+			"data":  json,
+		})
+	})
+
+	api.Delete("/comment/:id", func(c *fiber.Ctx) error {
+		if err := db.Delete(&model.Comment{}, c.Params("id")).Error; err != nil {
 			return err
 		}
 
--- model/project.go
+++ model/project.go
@@ -12,14 +12,14 @@ type Project struct {
 
 type Record struct {
 	gorm.Model
-	Title       string `gorm:"size:50;index" binding:"required" json:"title"`
-	Description string `gorm:"size:120" json:"description"`
-	Status      uint   `gorm:"index" json:"status" desc:"提议1, 通过2, 完成3, 已弃用4, 已取代5"`
-	ProjectId   uint   `json:"project_id" binding:"required"`
+	Title       string `gorm:"size:50;index" binding:"required" json:"title" validate:"required,min=3,max=50"`
+	Description string `gorm:"type:text" json:"description" validate:"required"`
+	Status      uint   `gorm:"index" json:"status" desc:"提议1, 通过2, 完成3, 已弃用4, 已取代5" validate:"required,oneof=1 2 3 4 5"`
+	ProjectId   uint   `json:"project_id" binding:"required" validate:"required"`
 }
 
 type Comment struct {
 	gorm.Model
-	Description string `gorm:"type:text" json:"description"`
-	RecordId    uint   `json:"record_id"`
+	Description string `gorm:"type:text" json:"description" validate:"required"`
+	RecordId    uint   `json:"record_id" validate:"required"`
 }
